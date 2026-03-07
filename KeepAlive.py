b246f04a6bddad5b10af5978ceb5a135264ceb81
lazaisa <lazarisajlovic2@gmail.com>
2026-03-07 13:08:37 +0100

    Update KeepAlive.py

--- KeepAlive.py
+++ KeepAlive.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "Bot je budan!"
+    return "King is Alive! 👑"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    # Render zahteva da aplikacija slusa na portu koji oni dodele
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
+    t.daemon = True # Ovo osigurava da se thread gasi kad i glavni proces
     t.start()
